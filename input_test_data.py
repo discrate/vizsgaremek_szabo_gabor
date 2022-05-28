1e5bc0009a6b30a13d870796b045f9528ecdb142
discrate <88935250+discrate@users.noreply.github.com>
2022-05-28 15:14:09 +0200

    update TE 1-4

--- input_test_data.py
+++ input_test_data.py
@@ -10,5 +10,12 @@ user2 = {
     "password": "Tesztelek2"
 }
 
+# növelni kellene a username-et és az emailcím számát eggyel! password maradhat
+user3 = {
+    "name": "szgtest",
+    "email": "szgtest@gmail.com",
+    "password": "Tesztelek1"
+}
+
 # Alap profilkép:
 # https://static.productionready.io/images/smiley-cyrus.jpg
--- main.py
+++ main.py
@@ -12,29 +12,107 @@ browser.maximize_window()
 
 # browser.quit()
 
-def sign_in():
-    home_sign_in_btn = browser.find_elements_by_xpath('//a[@href="#/login"]')[0]
-    # sign_in_btn = browser.find_elements_by_xpath('//a[normalize-space()="Sign in"]')[0]
-    home_sign_in_btn.click()
-    email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
-    email_input.send_keys(user1["email"])
-    password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
-    password_input.send_keys(user1["password"])
-    sign_in_btn = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
-    sign_in_btn.click()
-    time.sleep(2)
-    user_profile = browser.find_elements_by_xpath('//a[@class="nav-link"]')[2]
-    # user_profile = browser.find_element_by_xpath('//a[@class="nav-link"][normalize-space()="szgteszt1"]')
-    assert user_profile.text == user1["name"]  # helyes felhasználónév megjelenítésének ellenőrzése
+# Test eset 01 -- Regisztráció helytelen adatokkal
+# def registration_invalid():
+#     sign_up_btn = browser.find_element_by_xpath('//a[@href="#/register"]')
+#     sign_up_btn.click()
+#     username_input = browser.find_element_by_xpath('//input[@placeholder="Username"]')
+#     email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+#     password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+#     sign_up_send_btn = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+#     username_input.send_keys(user3["name"])
+#     email_input.send_keys("incorrect")
+#     password_input.send_keys(user3["password"])
+#     sign_up_send_btn.click()
+#     time.sleep(2)
+#     result_message = browser.find_element_by_xpath('//div[@class="swal-title"]')
+#     result_reason = browser.find_element_by_xpath('//div[@class="swal-text"]')
+#     try:
+#         assert result_message.text == "Registration failed!"
+#         assert result_reason.text == "Email must be a valid email."
+#         print('Helyes hibaüzenet')
+#     except AssertionError:
+#         print('Helytelen validáció')
+
+# registration_invalid()
 
 
-def logout():
-    logout_btn = browser.find_element_by_xpath('//a[@active-class="active"]')
-    logout_btn.click()
-    home_sign_in_btn = browser.find_elements_by_xpath('//a[@href="#/login"]')[0]
-    assert home_sign_in_btn.text == "Sign in"
+# Test eset 02 -- Regisztráció helyes adatokkal
+# def registration_valid():
+#     sign_up_btn = browser.find_element_by_xpath('//a[@href="#/register"]')
+#     sign_up_btn.click()
+#     username_input = browser.find_element_by_xpath('//input[@placeholder="Username"]')
+#     email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+#     password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+#     sign_up_send_btn = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+#     username_input.send_keys("szgteszt998")
+#     email_input.send_keys("szgteszt998@gmail.com")
+#     password_input.send_keys(user3["password"])
+#     sign_up_send_btn.click()
+#     time.sleep(2)
+#     result_message = browser.find_element_by_xpath('//div[@class="swal-title"]')
+#     result_reason = browser.find_element_by_xpath('//div[@class="swal-text"]')
+#     try:
+#         assert result_message.text == "Welcome!"
+#         assert result_reason.text == "Your registration was successful!"
+#         print('Sikeres regisztráció')
+#     except AssertionError:
+#         print('Sikertelen regisztráció')
+#
+#     ok_btn = browser.find_element_by_xpath('//button[@class="swal-button swal-button--confirm"]')
+#     ok_btn.click()
+#
+# registration_valid()
 
 
-sign_in()
-time.sleep(1)
-logout()
+# Test eset 03 -- Bejelentkezés
+# def sign_in():
+#     home_sign_in_btn = browser.find_elements_by_xpath('//a[@href="#/login"]')[0]
+#     # sign_in_btn = browser.find_elements_by_xpath('//a[normalize-space()="Sign in"]')[0]
+#     home_sign_in_btn.click()
+#     email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+#     email_input.send_keys(user1["email"])
+#     password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+#     password_input.send_keys(user1["password"])
+#     sign_in_btn = browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+#     sign_in_btn.click()
+#     time.sleep(2)
+#     user_profile = browser.find_elements_by_xpath('//a[@class="nav-link"]')[2]
+#     # user_profile = browser.find_element_by_xpath('//a[@class="nav-link"][normalize-space()="szgteszt1"]')
+#     try:
+#         assert user_profile.text == user1["name"]  # helyes felhasználónév megjelenítésének ellenőrzése
+#         print('Sikeres bejelentkezés')
+#     except AssertionError:
+#         print('Nem sikerült bejelentkezni')
+
+
+# Test eset 04 -- Adatkezelési nyilatkozat használata
+def accept_cookies():
+    accept_btn = browser.find_element_by_xpath('//div[normalize-space()="I accept!"]')
+    accept_btn.click()
+    time.sleep(1)
+    decline_btn_list = browser.find_elements_by_xpath('//div[normalize-space()="I decline!"]')
+    print(len(decline_btn_list))
+    assert len(decline_btn_list) == 0
+
+
+accept_cookies()
+
+# //div[normalize-space()='I decline!']
+# //div[normalize-space()='I accept!']
+
+# # Test eset 12 -- Kijelentkezés       # már bejelentkezett a user és elég csak logoutolni vagy login-t is meg kell hívni előtte???
+# def logout():
+#     logout_btn = browser.find_element_by_xpath('//a[@active-class="active"]')
+#     logout_btn.click()
+#     home_sign_in_btn = browser.find_elements_by_xpath('//a[@href="#/login"]')[0]
+#     try:
+#         assert home_sign_in_btn.text == "Sign in"
+#         print('Sikeres kijelentkezés')
+#     except AssertionError:
+#         print('Nem sikerült kijelentkezni')
+#
+#
+# sign_in()
+# time.sleep(1)
+# logout()
