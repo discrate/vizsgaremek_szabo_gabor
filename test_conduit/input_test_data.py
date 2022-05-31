9068db0abc412926ae022587e3b4e0bea7d7015a
discrate <88935250+discrate@users.noreply.github.com>
2022-05-31 19:04:22 +0200

    update inputs

--- test_conduit/input_test_data.py
+++ test_conduit/input_test_data.py
@@ -1,13 +1,18 @@
 user1 = {
     "name": "szgteszt1",
     "email": "szgteszt1@gmail.com",
-    "password": "Tesztelek1"
+    "password": "Tesztelek1",
+    "article1": "Nekünk kell egymással és önmagunk lelkéből tüzet rakni. Hogy melegedhessünk körülötte. Ott, ahol akármilyen kis körben, "
+                "de szeretet van, jó lesz élni. Ott meghittség lesz. Barátságos világ. Boldogító légkör.",
+    "comment1": "ez egy világos hozzászólás",
 }
 
 user2 = {
     "name": "szgteszt2",
     "email": "szgteszt2@gmail.com",
-    "password": "Tesztelek2"
+    "password": "Tesztelek2",
+    "article2":"Az összevisszaságban találd meg az egyszerűséget, a hangzavarban a harmóniát. A nehézségek közt mindig ott van a lehetőség.",
+    "comment2": "ez egy értelmes hozzászólás",
 }
 
 user = {
